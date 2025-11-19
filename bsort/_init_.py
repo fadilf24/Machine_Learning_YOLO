72be3ac92776050a31abc5506eaebe6559d9a471
Fadil Farhan Ridwan <98074501+fadilf24@users.noreply.github.com>
2025-11-19 20:32:50 +0700

    Update _init_.py

--- bsort/_init_.py
+++ bsort/_init_.py
@@ -1,6 +1,6 @@
-"""
-bsort package initialization.
-Provides CLI, training, inference, and utilities for bottle cap detection.
-"""
+#"""
+#bsort package initialization.
+#Provides CLI, training, inference, and utilities for bottle cap detection.
+#"""
 
-__version__ = "1.0.0"
+#__version__ = "1.0.0"
